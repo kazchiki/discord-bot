408c87cb4639d50b4f0af6812d44634656265211
kazuki <kazuki-ichiki@cal.co.jp>
2026-01-26 11:23:18 +0900

    fix: サービスのインポートを修正
    削除したファイルのインポートを修正

--- genshin/services/__init__.py
+++ genshin/services/__init__.py
@@ -4,13 +4,11 @@ Service Layer - ビジネスロジック
 """
 
 from .hoyolab_service import HoyolabService
-from .resin_service import ResinService
 from .team_service import TeamService
 from .notification_service import NotificationService
 
 __all__ = [
     'HoyolabService',
-    'ResinService',
     'TeamService',
     'NotificationService'
 ]
